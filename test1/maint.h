74210fd7c743668712de386f9519c1eb9b8873f7
AFOP <4592@holbertonschool.com>
2022-03-22 12:28:05 -0700

    Update test one

---
+++ test1/maint.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct specifier - Struct format
+ *
+ * @f: function
+ * @list: The list of the function
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
+
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
