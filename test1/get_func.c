8b02313cbeae780785c2f73d0ace9ab10c5e212b
AFOP <4592@holbertonschool.com>
2022-03-24 16:46:22 -0700

    Copy to test1 printf task0

--- test1/get_func.c
+++ test1/get_func.c
@@ -4,27 +4,25 @@
 #include "main.h"
 /**
  * get_func - get and compare
- * @x: type of format
+ * @format: pointer char
  * Return: pointer to function that returns struct of type printable
  */
-int (*get_func(char x))(va_list)
-
+int (*get_func(const char *format))(va_list)
 {
 	int i = 0;
 
 	spec arr[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"%", print_percent},
 		{NULL, NULL}
 	};
-	while (i < 3)
+	while (arr[i].valid != NULL)
 	{
-		if (x == arr[i].valid[0])
+		if (*(arr[i].valid) == *format)
 		{
-			return (arr[i].f);
+			break;
 		}
-			i++;
+		i++;
 	}
-	return (0);
+	return (arr[i].f);
 }
--- test1/printf.c
+++ test1/printf.c
@@ -9,45 +9,44 @@
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i = 0, (*m)(va_list);
-		va_list args;
+	int count  = 0, i = 0;
+	int (*m)(va_list);
+	va_list args;
 
-		va_start(args, format);
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]); i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-					{
-						if (format[i + 1] == 'c')
-							count += print_c(args);
-						if (format[i + 1] == 's')
-							count += print_s(args);
-					}
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]); 
-				i++;
-			}
+			_putchar(format[i]);
+			count++;
 		}
-		va_end(args); 
-		return (count);
+		if (!format[i])
+			return (count);
+		m = get_func(&format[i + 1]);
+		if (m != NULL)
+		{
+			count += m(args);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+		{
+			return (-1);
+		}
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+		{
+			i += 2;
+		}
+		else
+		{
+			i++;
+		}
 	}
-	return (-1);
+	va_end(args);
+	return (count);
 }
